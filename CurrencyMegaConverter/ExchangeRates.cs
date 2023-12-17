6f8e6ed9f2e2fa90ddfe187a8d44b83a5e6f5439
diMASasin <35368058+diMASasin@users.noreply.github.com>
2023-12-17 16:14:36 +0500

    Добавьте файлы проекта.

---
+++ CurrencyMegaConverter/ExchangeRates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyMegaConverter
+{
+    public class ExchangeRates
+    {
+        public DateTime Date { get; set; }
+        public DateTime PreviousDate { get; set; }
+        public string PreviousURL { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Dictionary<string, ValuteItem> Valute { get; set; }
+    }
+
+    public class ValuteItem
+    {
+        public string ID { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public double Previous { get; set; }
+    }
+}
---
+++ CurrencyMegaConverter/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CurrencyMegaConverter 
+{
+    internal class MainViewModel : BindableObject
+    {
+        private string[] _currencies = { "shish", "shoah"};
+
+        public string[] Currencies
+        {
+            get => _currencies;
+            set
+            {
+                _currencies = value;
+                OnPropertyChanged(nameof(Currencies));
+            }
+        }
+
+        private string _current;
+
+        public string Current
+        {
+            get => _current;
+            set
+            {
+                _current = value;
+                OnPropertyChanged(nameof(Current));
+            }
+        }
+
+        private ExchangeRates _exchangeRates;
+
+        public MainViewModel()
+        {
+            var uri = new Uri($"https://www.cbr-xml-daily.ru/daily_json.js");
+            var client = new HttpClient();
+            var response = client.GetAsync(uri).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            _exchangeRates = JsonSerializer.Deserialize<ExchangeRates>(result);
+            Currencies = _exchangeRates.Valute.Keys.ToArray();
+            Current = Currencies[0];
+        }
+    }
+}
