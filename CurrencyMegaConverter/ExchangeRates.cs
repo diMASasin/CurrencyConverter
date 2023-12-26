623c830b2eb9af0d4d52f83477069484c227aef5
diMASasin <35368058+diMASasin@users.noreply.github.com>
2023-12-26 19:30:14 +0500

    Converter

--- CurrencyMegaConverter/ExchangeRates.cs
+++ CurrencyMegaConverter/ExchangeRates.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CurrencyMegaConverter
 {
-    public class ExchangeRates
+    public class ExchangeRates : BindableObject
     {
-        public DateTime Date { get; set; }
+        private DateTime _date;
+
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                DatePicked?.Invoke();
+                DateText = _date.ToString();
+                OnPropertyChanged();
+            }
+        }
+
+        public event Action DatePicked;
+
         public DateTime PreviousDate { get; set; }
         public string PreviousURL { get; set; }
         public DateTime Timestamp { get; set; }
         public Dictionary<string, ValuteItem> Valute { get; set; }
+
+        private string _dataText;
+        public string DateText
+        {
+            get 
+            {
+                return "Показан курс на: " + _dataText;
+            }
+            set
+            {
+                _dataText = value;
+                OnPropertyChanged();
+            }
+        }
     }
 
     public class ValuteItem
@@ -24,5 +54,13 @@ namespace CurrencyMegaConverter
         public string Name { get; set; }
         public double Value { get; set; }
         public double Previous { get; set; }
+
+        public ValuteItem(string charCode, int nominal, string name, double value)
+        {
+            CharCode = charCode;
+            Nominal = nominal;
+            Name = name;
+            Value = value;
+        }
     }
 }
